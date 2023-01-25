b153dc6f836c0185b3363add2fefb05530f8b7db
Sowoon <janus7359@gmail.com>
2023-01-25 23:37:11 +0900

    일부 code syntax 수정

--- src/templates/post-page.tsx
+++ src/templates/post-page.tsx
@@ -18,7 +18,7 @@ interface Props extends PageProps {
     };
   };
   pageContext: {
-    contentful_id: string;
+    id: string;
     slug: string;
   };
 }
@@ -60,7 +60,7 @@ const PostPage: React.ComponentType<Props> = ({ data }) => {
 
         <div
           dangerouslySetInnerHTML={{ __html: html }}
-          className={`prose max-w-none prose-p:my-0.5 prose-code:rounded prose-code:p-1 prose-code:before:content-[''] prose-code:after:content-[''] dark:prose-code:bg-zinc-700`}
+          className={`prose max-w-none prose-p:my-0.5 prose-code:rounded prose-code:p-1 prose-code:before:content-[''] prose-code:after:content-[''] dark:prose-code:bg-zinc-700 prose-code:bg-zinc-100`}
         />
       </article>
     </Layout>
